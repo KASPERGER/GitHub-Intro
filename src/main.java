4026ddfd31ac90a058793c87585092e7dcf1ff10
KASPERGER <isac.olander.te16c@gmail.com>
2017-11-15 14:45:55 +0100

    Ändringar på makeview3

--- src/main.java
+++ src/main.java
@@ -44,7 +44,7 @@ public class main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 
 		// Ändra siffran här nere till 1,2,3 eller 4 beroende på vilken vy du vill se!
-		makeView2();
+		makeView3();
 
 		primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
 		primaryStage.show();
@@ -134,12 +134,14 @@ public class main extends Application {
 		 * 		 Sedan måste mellanrummet mellan ringarna minska också...
 		 */
 
-		int rings = 15;
+		int rings = 200;
 
+	
+		
 		for (int i = rings; i >= 0; i--) {
 
 			double radius = WIDTH / rings / 2;
-
+			
 			Circle cir = new Circle(i * radius);
 
 			int r = (int) (Math.random() * 255);
